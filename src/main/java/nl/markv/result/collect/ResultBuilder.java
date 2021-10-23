0e1c28953c8bbd33962b2d098aa3ffd696813917
Mark <mverleg.noreply@gmail.com>
2021-10-23 15:03:19 +0200

    Remove unused and untested methods from ResultBuilder

--- src/main/java/nl/markv/result/collect/ResultBuilder.java
+++ src/main/java/nl/markv/result/collect/ResultBuilder.java
@@ -1,8 +1,5 @@
 package nl.markv.result.collect;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-
 import javax.annotation.Nonnull;
 
 import nl.markv.result.Err;
@@ -19,24 +16,8 @@ final class ResultBuilder<T, E> {
 	}
 
 	@Nonnull
-	public static <T, E> ResultBuilder<T, E> from(@Nonnull Result<T, E> result) {
-		return new ResultBuilder<>(result);
-	}
-
-	@Nonnull
 	public static <T, E> ResultBuilder<T, E> ok(@Nonnull T value) {
 		return new ResultBuilder<>(Ok.of(value));
-	}
-
-	@Nonnull
-	public static <T, E> ResultBuilder<T, E> err(@Nonnull E value) {
-		return new ResultBuilder<>(Err.of(value));
-	}
-
-	@Nonnull
-	public ResultBuilder<T, E> toOk(@Nonnull T value) {
-		current = Ok.of(value);
-		return this;
 	}
 
 	@Nonnull
@@ -45,39 +26,13 @@ final class ResultBuilder<T, E> {
 		return this;
 	}
 
-	public boolean isOk() {
-		return current.isOk();
-	}
-
 	public boolean isErr() {
 		return current.isErr();
-	}
-
-	public void ifOk(@Nonnull Consumer<T> action) {
-		current.ifOk(action);
-	}
-
-	public void ifErr(@Nonnull Consumer<E> action) {
-		current.ifErr(action);
-	}
-
-	public void ifEither(@Nonnull Consumer<T> okAction, Consumer<E> errAction) {
-		current.ifEither(okAction, errAction);
-	}
-
-	@Nonnull
-	public <R> R branch(@Nonnull Function<T, R> okConverter, Function<E, R> errHandler) {
-		return current.branch(okConverter, errHandler);
 	}
 
 	@Nonnull
 	public T getOrThrow() {
 		return current.getOrThrow();
-	}
-
-	@Nonnull
-	public E getErrOrThrow() {
-		return current.getErrOrThrow();
 	}
 
 	@Nonnull
