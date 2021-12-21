c0ffee2e5c526d3ec253c7419c70ebc6d0bf0490
Mark <mverleg.noreply@gmail.com>
2021-12-22 00:10:42 +0100

    Build steps for fat and skinny jars (no release yet)

--- ci/merge_jar_sources.sh
+++ ci/merge_jar_sources.sh
@@ -2,19 +2,25 @@
 
 set -e -E -o pipefail -u -x
 
+if [ $# != 1 ]; then
+    echo 'single argument must be jar base name, e.g. "result-fat"' 1>&2
+    exit 1
+fi
+
+base="$1"
 cur="$(pwd)"
 ver="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout 2>/dev/null)"
 fd="$(mktemp -d)"
-printf 'version = %s, dir = %s\n' "$ver" "$fd"
+printf 'base = %s, version = %s, dir = %s\n' "$base" "$ver" "$fd"
 
-unzip -q -o "target/result-fat-${ver}-sources.jar" -d "$fd"
-unzip -q -o "target/result-fat-${ver}.jar" -d "$fd"
+unzip -q -o "target/${base}-${ver}-sources.jar" -d "$fd"
+unzip -q -o "target/${base}-${ver}.jar" -d "$fd"
 
 printf 'java file count: %s' "$(find "$fd" -name '*.java' | wc -l)"
 find "$fd" -name '*.java' -print -exec touch -d '1 hour ago' {} +
 find "$fd" -name '*.class' -exec touch -d 'now' {} +
 
-rm -f "${cur}/result-fat-${ver}.jar"
+rm -f "${cur}/${base}-${ver}.jar"
 cd "$fd"
-zip -q -r "${cur}/result-fat-${ver}.jar" .
+zip -q -r "${cur}/${base}-${ver}.jar" .
 
